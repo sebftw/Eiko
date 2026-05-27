3699cfad37b894a09c54a3b7b18406f7138bb19a
Sebastian Præsius <sebastian.devel@gmail.com>
2026-05-27 22:28:25 +0200

    Configure CUDAExtension in setup.py
    Added CUDA extension setup for Eiko project.

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from build_config import CXX_ARGS, NVCC_ARGS, EXTRA_INCLUDE_PATHS
 
-if __name__ == "__main__":
-    # Installation of Eiko is handled in pyproject.toml, not here.
-    setup()
+# Map source file relative to the project root
+sources = ["src/bindings/torch_bindings.cu"]
+
+setup(
+    ext_modules=[
+        CUDAExtension(
+            name="eiko.eiko_torch_impl",  # Important: Must match the import name!
+            sources=sources,
+            extra_compile_args={
+                "cxx": CXX_ARGS,
+                "nvcc": NVCC_ARGS,
+            },
+            include_dirs=EXTRA_INCLUDE_PATHS,
+        )
+    ],
+    cmdclass={
+        "build_ext": BuildExtension
+    }
+)
