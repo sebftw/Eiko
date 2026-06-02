d8f34d8876bf5387d2ec683257e331aa42ceb709
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-02 20:12:25 +0200

    Comment out C++ standard arguments in build_config.py

--- python/eiko/build_config.py
+++ python/eiko/build_config.py
@@ -6,19 +6,21 @@ import sys
 IS_RELEASE_BUILD = os.environ.get("EIKO_RELEASE_BUILD", "0") == "1"
 
 if sys.platform == "win32":
-    CXX_ARGS = ['/std:c++17', '/Zc:preprocessor', '/DNOMINMAX']
+    CXX_ARGS = [# '/std:c++17',
+                '/Zc:preprocessor', '/DNOMINMAX']
     NVCC_ARGS = [
-        '-std=c++17', 
+        # '-std=c++17', 
         '-allow-unsupported-compiler', 
         '-Xcompiler', '/Zc:preprocessor', 
-        '-Xcompiler', '/std:c++17', 
         '-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH', 
         '-DNOMINMAX', 
         '--use_fast_math'
     ]
 else:
-    CXX_ARGS = ['-std=c++17', '-O3']
-    NVCC_ARGS = ['-std=c++17', '--use_fast_math']
+    CXX_ARGS = [#'-std=c++17',
+                '-O3']
+    NVCC_ARGS = [#'-std=c++17', 
+                 '--use_fast_math']
 
 if IS_RELEASE_BUILD:
     NVCC_ARGS.append('-arch=all-major')
