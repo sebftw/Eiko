70bde3632bacedcbab43cc1a2637a4f16a23c7b7
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-03 03:26:22 +0200

    Update CXX_ARGS and NVCC_ARGS for Windows build

--- python/eiko/build_config.py
+++ python/eiko/build_config.py
@@ -6,13 +6,13 @@ import sys
 IS_RELEASE_BUILD = os.environ.get("EIKO_RELEASE_BUILD", "0") == "1"
 
 if sys.platform == "win32":
-    CXX_ARGS = [# '/std:c++17',
+    CXX_ARGS = ['/permissive',
                 '/Zc:preprocessor', '/DNOMINMAX', '/wd3189']
     NVCC_ARGS = [
-        # '-std=c++17', 
         '-allow-unsupported-compiler', 
         '-Xcompiler', '/Zc:preprocessor', 
         '-Xcompiler', '/wd3189', 
+        '-Xcompiler=/permissive',
         '-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH', 
         '-DNOMINMAX', 
         '--use_fast_math'
