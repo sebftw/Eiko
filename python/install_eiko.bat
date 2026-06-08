4ad88aaad53bc0d670532295b2f79c10452ab1e2
Sebastian Kazmarek Præsius <sebastian.devel@gmail.com>
2026-06-08 03:08:27 +0200

    Updated script (almost finished)

--- python/install_eiko.bat
+++ python/install_eiko.bat
@@ -3,15 +3,11 @@
 :; # Works on Linux Bash (via source <(curl...)) and Windows (via double-click or cmd)
 :; # ==============================================================================
 :; # Linux/Bash Section
-:; source <(wget -qO- https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_ubuntu.sh)
-:; exit $?
+:; source <(wget -qO- https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_ubuntu.sh) #
+:; exit $? #
 
 @echo off
 REM Windows Command Prompt / Batch Section
-echo ====================================================
-echo  Starting Eiko Smart Environment Setup (Windows)   
-echo ====================================================
-
 powershell -NoExit -ExecutionPolicy Bypass -Command "$script = (Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_windows.ps1').Content; . ([ScriptBlock]::Create($script))"
 if %errorlevel% neq 0 (
     echo [!] Windows Installation failed.
