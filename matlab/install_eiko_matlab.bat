faef3d38a0d49535931fe8aa6022f643d55b5a42
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-13 17:28:57 +0200

    Update install_eiko_matlab.bat

--- matlab/install_eiko_matlab.bat
+++ matlab/install_eiko_matlab.bat
@@ -12,7 +12,7 @@
 :; # SOFTWARE.
 :; # ==============================================================================
 :; # Linux/Bash Section
-:; if [ ! -f "install_eiko_ubuntu.sh" ]; then #
+:; if [ ! -f "install_eiko_matlab_ubuntu.sh" ]; then #
 :;   echo "Downloading Eiko repository..." #
 :;   URL="https://github.com/sebftw/Eiko/archive/refs/heads/main.tar.gz" #
 :;   if command -v curl >/dev/null 2>&1; then #
@@ -32,15 +32,15 @@
 :;   mv Eiko-main Eiko #
 :;   cd Eiko/matlab || { echo "Failed to find directory"; exit 1; } #
 :; else #
-:;   echo "Local install_eiko_ubuntu.sh found. Skipping download." #
+:;   echo "Local install_eiko_matlab_ubuntu.sh found. Skipping download." #
 :; fi #
-:; chmod +x install_eiko_ubuntu.sh #
-:; bash ./install_eiko_ubuntu.sh #
+:; chmod +x install_eiko_matlab_ubuntu.sh #
+:; bash ./install_eiko_matlab_ubuntu.sh #
 :; read -n 1 -s -r -p "Installation finished. Press any key to exit..." #
 :; exit $? #
 @echo off
 REM Windows Command Prompt / Batch Section
-powershell -NoExit -ExecutionPolicy Bypass -Command "if (Test-Path '.\install_eiko_windows.ps1') { Write-Host 'Local installer found. Skipping download.' } else { $ProgressPreference = 'SilentlyContinue'; Write-Host 'Downloading Eiko repository...'; Invoke-WebRequest -Uri 'https://github.com/sebftw/Eiko/archive/refs/heads/main.zip' -OutFile 'Eiko-main.zip'; Write-Host 'Extracting repository...'; Expand-Archive -Path 'Eiko-main.zip' -DestinationPath '.' -Force; Remove-Item -Path 'Eiko-main.zip' -Force; if (Test-Path 'Eiko') { Remove-Item 'Eiko' -Recurse -Force }; Rename-Item -Path 'Eiko-main' -NewName 'Eiko'; Set-Location -Path 'Eiko\matlab' } .\install_eiko_windows.ps1"
+powershell -NoExit -ExecutionPolicy Bypass -Command "if (Test-Path '.\install_eiko_matlab_windows.ps1') { Write-Host 'Local installer found. Skipping download.' } else { $ProgressPreference = 'SilentlyContinue'; Write-Host 'Downloading Eiko repository...'; Invoke-WebRequest -Uri 'https://github.com/sebftw/Eiko/archive/refs/heads/main.zip' -OutFile 'Eiko-main.zip'; Write-Host 'Extracting repository...'; Expand-Archive -Path 'Eiko-main.zip' -DestinationPath '.' -Force; Remove-Item -Path 'Eiko-main.zip' -Force; if (Test-Path 'Eiko') { Remove-Item 'Eiko' -Recurse -Force }; Rename-Item -Path 'Eiko-main' -NewName 'Eiko'; Set-Location -Path 'Eiko\matlab' } .\install_eiko_matlab_windows.ps1"
 if %errorlevel% neq 0 (
     echo [!] Windows Installation failed.
     pause
