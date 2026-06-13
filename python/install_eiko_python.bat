a8b8d8554ec7b02e95b610c6fc60b93790e1c639
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-13 17:26:47 +0200

    Update install_eiko_python.bat

--- python/install_eiko_python.bat
+++ python/install_eiko_python.bat
@@ -11,24 +11,24 @@
 :; # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 :; # SOFTWARE.
 :; # ==============================================================================
-:; if [ ! -f "install_eiko_ubuntu.sh" ]; then
+:; if [ ! -f "install_eiko_python_ubuntu.sh" ]; then
 :;   echo "Installer not found locally. Downloading..."
-:;   URL="https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_ubuntu.sh"
-:;   if command -v curl >/dev/null 2>&1; then curl -sL "$URL" -o install_eiko_ubuntu.sh
-:;   elif command -v wget >/dev/null 2>&1; then wget -qO install_eiko_ubuntu.sh "$URL"
+:;   URL="https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_python_ubuntu.sh"
+:;   if command -v curl >/dev/null 2>&1; then curl -sL "$URL" -o install_eiko_python_ubuntu.sh
+:;   elif command -v wget >/dev/null 2>&1; then wget -qO install_eiko_python_ubuntu.sh "$URL"
 :;   else echo "Error: Neither curl nor wget found." && exit 1; fi
 :; fi
-:; chmod +x install_eiko_ubuntu.sh
-:; bash ./install_eiko_ubuntu.sh
+:; chmod +x install_eiko_python_ubuntu.sh
+:; bash ./install_eiko_python_ubuntu.sh
 :; read -n 1 -s -r -p "Installation finished. Press any key to exit..."
 :; exit $?
 
 @echo off
 REM Windows Command Prompt Section
-set "SCRIPT_NAME=install_eiko_windows.ps1"
+set "SCRIPT_NAME=install_eiko_python_windows.ps1"
 if not exist "%SCRIPT_NAME%" (
     echo Installer not found locally. Downloading...
-    powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_windows.ps1' -OutFile '%SCRIPT_NAME%'"
+    powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/sebftw/Eiko/main/python/install_eiko_python_windows.ps1' -OutFile '%SCRIPT_NAME%'"
 )
 powershell -ExecutionPolicy Bypass -Command ". .\%SCRIPT_NAME%"
 if %errorlevel% neq 0 (
