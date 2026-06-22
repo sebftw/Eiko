d09a124d784cc40489e8563251526b0ed0dfc51a
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-22 18:53:57 +0200

    Update install_eiko_python_ubuntu.sh

--- python/install_eiko_python_ubuntu.sh
+++ python/install_eiko_python_ubuntu.sh
@@ -366,10 +366,11 @@ if [ "$PY_VER" == "3.8" ]; then
         uninstall_if_present "torch" "torchvision" "torchaudio"
         echo -e "${MAGENTA}  -> Installing JAX stack...${NC}"
         run_pip_command install $TARGET_JAX_VER --no-input -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html || safe_exit 1
-        run_pip_command install "pybind11" --no-input || safe_exit 1
+        run_pip_command install pybind11 --no-input || safe_exit 1
     elif [ "$choice" == "2" ]; then
         if ! is_torch_valid; then
             run_pip_command install $TARGET_TORCH_VER --index-url $TARGET_WHEEL_URL --no-input || safe_exit 1
+			run_pip_command install ninja --no-input || safe_exit 1
         fi
         run_pip_command install "eiko" --no-input || safe_exit 1
     else
@@ -379,6 +380,7 @@ else
     # Logic for modern Python: install everything
     if ! is_torch_valid; then
         run_pip_command install $TARGET_TORCH_VER --index-url $TARGET_WHEEL_URL --no-input || safe_exit 1
+		run_pip_command install ninja --no-input || safe_exit 1
     fi
     run_pip_command install $TARGET_JAX_VER --no-input -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html || safe_exit 1
     run_pip_command install "pybind11" --no-input || safe_exit 1
