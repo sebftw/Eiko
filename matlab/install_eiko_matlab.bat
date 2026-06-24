96720a1c2b7e12543c23d8cec6c98871bfb5f608
Sebastian Præsius <sebastian.devel@gmail.com>
2026-06-24 16:57:12 +0200

    Add shebang for cross-platform compatibility

--- matlab/install_eiko_matlab.bat
+++ matlab/install_eiko_matlab.bat
@@ -1,3 +1,4 @@
+#!/bin/sh
 :; # ==============================================================================
 :; # Eiko Cross-Platform Unified Installer Polyglot Script
 :; # Works on Linux (via "bash install_eiko_matlab.bat") and Windows (via double-click)
